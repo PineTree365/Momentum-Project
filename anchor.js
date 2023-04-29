1c0f927e51f3b7da1ca5ba0d33f312a81380941d
PineTree365 <130726479+PineTree365@users.noreply.github.com>
2023-04-29 21:48:12 +0900

    update 4

--- anchor.js
+++ anchor.js
@@ -3,9 +3,10 @@ const createBtn = document.querySelector(".anchor-zone_create");
 const addShortcutForm = document.querySelector(".add-shortCut_form");
 const wholeContent = document.querySelector("#wholeContent");
 const anchorSection = document.querySelector(".anchor-zone_section");
-const ellipsis = document.querySelector(".ellipsis button");
+const ellipsis = anchorSection.querySelector("button");
+const createBox = anchorZone.querySelector("form");
 
-function addShortcut(event) {
+function showAddShortcut(event) {
     event.preventDefault();
     addShortcutForm.classList.remove("invisivle");
     wholeContent.classList.add("blur");
@@ -26,5 +27,41 @@ function ellipsisHidden() {
     ellipsis.classList.add("hidden");
 }
 
-createBtn.addEventListener("click", addShortcut);
-anchorSection.addEventListener("mouseenter",reveal);
+function createBoxReveal() {
+    createBox.classList.remove("hidden");
+    anchorSection.addEventListener("mouseleave",createBoxReveal);
+}
+function createBoxReveal() {
+    createBox.classList.add("hidden");
+}
+
+function replaceShortcutInfo () {}
+
+createBtn.addEventListener("click", showAddShortcut);
+anchorSection.addEventListener("mouseenter",ellipsisReveal);
+anchorSection.addEventListener("contextmenue",replaceShortcutInfo);
+anchorZone.addEventListener("mouseenter",createBoxReveal);
+
+const shortcutUrl = document.querySelector("#add-shortCut_form__url");
+const shortcutName = document.querySelector("#add-shortCut_form__name");
+
+function addShortcut(event) {
+    event.preventDefault();
+    const url = shortcutUrl.value;
+    const name = shortcutName.value;
+    shortcutUrl.value ="";
+    shortcutName.value ="";
+    const div = document.createElement("div");
+    const a = document.createElement("a");
+    const span = document.createElement("span");
+    span.innerText = name;
+    div.classList.add("anchor-zone_section");
+    div.appendChild(a);
+    a.classList.add("anchor-stlyle");
+    a.href = url;
+    a.appendChild(span);
+    anchorZone.prepend(div); 
+
+}
+
+addShortcutForm.addEventListener("submit", addShortcut);
